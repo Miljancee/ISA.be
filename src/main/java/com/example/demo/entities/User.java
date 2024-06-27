4561d43291bd5b7e449e7fc4922b4ff3160e1ff4
Miljancee <miljannedeljkovic02@gmail.com>
2024-06-28 00:58:50 +0200

    ISA - Project

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -2,31 +2,65 @@ package com.example.demo.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
+
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Integer id;
-
     @Column(name="first_name")
     private String firstName;
-
     @Column(name="last_name")
     private String lastName;
-
     @Column(name="email")
     private String email;
-
     @Column(name="contact_number")
     private String contactNumber;
+    @Column(name="password")
+    private String password;
 
     @OneToMany(mappedBy = "userId")
     private List<Product> products;
 
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
